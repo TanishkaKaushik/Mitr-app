67e515169d888e4c15b206d27e43e2f74accb178
TanishkaKaushik <takshu.kaushik@gmail.com>
2021-07-28 17:29:57 +0530

    first commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    MONGOURI:"mongodb+srv://Tanishkak19:UwnAfabOyVd58EzR@cluster0.o8kmc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET:"abcdefghijklmnop"
+
+
+}
+
