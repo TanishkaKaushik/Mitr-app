49ce593985583a891a5e91bc9cca9509851fea49
TanishkaKaushik <87811283+TanishkaKaushik@users.noreply.github.com>
2021-08-02 13:01:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 const mongoose  = require('mongoose')
 const {MONGOURI} = require('./keys')
 
@@ -28,7 +28,13 @@ app.use(require('./routes/user'))
 
 
 
-
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path= require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
 })
